7f6937a113460ddf991b63cf2ea011ce7dabc57a
DanielWFetterman <dwf5277@psu.edu>
2023-09-21 13:15:44 -0400

    almost done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -6,15 +5,43 @@ function App() {
 
     return (
       <div className="App">
-        <h1>Hello World!</h1>
+        {
+        
+        <div class="card">
+
+          <title>This card has a button and a image</title>
+        
+          <p>The image is a meme and says "I know HTML How to meet ladies"</p>
+          
+          <img id="HTML img" src="https://images.fineartamerica.com/images/artworkimages/mediumlarge/3/i-know-html-how-to-meet-ladies-funny-web-designer-print-noirty-designs.jpg" alt="HTML is fun"/>
+
+            
+            <a href="http://hax.psu.edu" target="http://hax.psu.edu">
+              <button type="button" id="btn-details"> Details </button> 
+            </a>  
+
+            
+
+            /* duplicate
+             change name 
+             change color
+             delete 
+             
+             */
+          
+
+        </div>
+          
+        
+        /* <h1>Hello World!</h1>
         <h1>Welcome to my card</h1>
         <img src="https://cdn.creatureandcoagency.com/uploads/2021/03/FrogLifecycle_Body_8.png" alt="Frog" />
         <div className="paragraph">
           <p>This is a picture of a frog that I found on the internet. It looks really cool, and I thought it would be nice to share.</p>
-        </div>
+        </div>n
         <div class="btn-wrapper">
  <a href="https://hax.psu.edu">
-   <button>Details</button> </a> </div>
+   <button>Details</button> </a> </div> */}
       </div>
     );
   }
